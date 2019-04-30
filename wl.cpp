77e98e2956c97e2fe5c45e7a58affdf10eda0216
Mihich <scimok1@gmail.com>
2019-04-30 20:41:11 +0700

    Added vocabulary made

--- wl.cpp
+++ wl.cpp
@@ -3,12 +3,18 @@
 #include <stdlib.h>
 #define n 10
 
+struct sl{char en[15]; char ru[15];} d;
+
+typedef struct sl s2;
+
+void Voc(s2 *w);
+
 main()
 { 	
 	int i, r=0,j=0;
   	char ch, eng[15], rus[15];
   	FILE *t;
-  	struct sl{char en[15]; char ru[15];} d;  
+  	  
   	struct sl w[n];  
   	t=fopen("file1.txt","r"); 
   
@@ -41,7 +47,41 @@ main()
 		i++;
 		
   	}
-	printf("%s = %s", w[4].en, w[4].ru);
-	printf("123");	
+	Voc(w);
+	
 	return 0;
+}
+
+
+void Voc(s2 *w){
+	int i,j,g,B[n],mo;
+	scanf("%d",&mo);
+	
+	for (i=0;i<n;i++) B[i]=i;
+
+	
+	if(mo==1) {
+		for(i=1;i<n;i++){
+		g=B[i];
+		j=i-1;
+		while(j>=0 and strcmp(w[g].en,w[B[j]].en)<0){
+			B[j+1]=B[j];
+			j=j-1;
+		}
+		B[j+1]=g;
+	}
+		for(i=0;i<n;i++) printf("%s - %s",w[B[i]].en,w[B[i]].ru);}
+	
+	if(mo==2) {
+		for(i=1;i<n;i++){
+		g=B[i];
+		j=i-1;
+		while(j>=0 and strcmp(w[g].ru,w[B[j]].ru)<0){
+			B[j+1]=B[j];
+			j=j-1;
+		}
+		B[j+1]=g;
+	} 
+		for(i=0;i<n;i++) printf("%s - %s\n",w[B[i]].ru,w[B[i]].en);}
+	
 }
