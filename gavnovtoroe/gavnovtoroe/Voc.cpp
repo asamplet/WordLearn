7179f3beb168d17f3755af9ab7642747610aef4d
Daria Podolyako <dasha_pod01@mail.ru>
2019-05-30 02:35:02 +0700

    Change some files for graphics's work

--- gavnovtoroe/gavnovtoroe/Voc.cpp
+++ gavnovtoroe/gavnovtoroe/Voc.cpp
@@ -1,27 +1,29 @@
-#include "WordLearn.h"
+#include "wordlearn.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include "pch.h"
+#include <string.h>
 #pragma warning(disable : 4996)
 #pragma warning(disable : 4430)
-#define n 30
+#define n 10
 
-void Voc(s2 *w) {
-	int i, j, g, B[n];
+void Voc(s2* w)
+{
+    int i, j, g, B[n];
 
-	for (i = 0; i < n; i++) B[i] = i;
+    for (i = 0; i < n; i++)
+        B[i] = i;
 
-	for (i = 1; i < n; i++) {
-		g = B[i];
-		j = i - 1;
-		while (j >= 0 and strcmp(w[g].en, w[B[j]].en) < 0) {
-			B[j + 1] = B[j];
-			j = j - 1;
-		}
-		B[j + 1] = g;
-	}
-	//for (i = 0; i < n; i++) printf("%s - %s\n", w[B[i]].en, w[B[i]].ru);
-
-
+    for (i = 1; i < n; i++) 
+	{
+        g = B[i];
+        j = i - 1;
+        while (j >= 0 and strcmp(w[g].en, w[B[j]].en) < 0) 
+		{
+            B[j + 1] = B[j];
+            j = j - 1;
+        }
+        B[j + 1] = g;
+    }
+    //for (i = 0; i < n; i++)
+      //  printf("%s - %s\n", w[B[i]].en, w[B[i]].ru);
 }
